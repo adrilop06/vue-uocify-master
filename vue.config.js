e41b306b1510b622a3aefc7347a95ffd98cfa63f
Adrian Lopez <adrianlopezpazos@gmail.com>
2021-01-01 15:25:02 -0600

    v1.1

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,28 @@ module.exports = {
         prependData: `@import "@/styles/_variables.scss";`
       }
     }
+  },
+  pwa: {
+    name: 'Uoccify',
+    themeColor: '#73EDFF',
+    msTileColor: '#222222',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {  
+      swSrc: 'src/register-service.js',
+    },
+    iconPaths:{
+      favicon32: 'public/img/icons/favicon-32x32.png',
+      favicon16: 'public/img/icons/favicon-16x16.png',
+      appleTouchIcon: 'public/img/icons//apple-touch-icon-152x152.png',
+      maskIcon: null,
+      msTileImage: 'public/img/icons/msapplication-icon-144x144.png'
+    },
+   
+    
   }
+  
 };
