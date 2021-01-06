b28070acb37357f9914f9429e1dee4ab966f5932
Adrian Lopez <adrianlopezpazos@gmail.com>
2021-01-06 09:18:27 -0600

    a

--- src/api.js
+++ src/api.js
@@ -1,44 +1,44 @@
 
 
 export async function getPlayLists(){ 
-  let t = await fetch("https://api.deezer.com/chart/0/playlists")
+  let t = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/playlists")
     let r = await t.json()
     return r.data
 }
 
 export async function getPlayList(param){ 
-    let t = await fetch("https://api.deezer.com/playlist/" + param + "/tracks")
+    let t = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/" + param + "/tracks")
     let r = await t.json()
     return r.data
 }
 
 export async function getTrack(param){ 
-    let t = await fetch("https://api.deezer.com/search/track?q=" + param)
+    let t = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/track?q=" + param)
     let r = await t.json()
     return r.data
 }
 export async function getAlbums(param){ 
-    let t = await fetch("https://api.deezer.com/search/album?q=" + param)
+    let t = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/album?q=" + param)
     let r = await t.json()
     return r.data
 }
 export async function getArtists(param){ 
-    let t = await fetch("https://api.deezer.com/search/artist?q=" + param)
+    let t = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/artist?q=" + param)
     let r = await t.json()
     return r.data
 }   
 export async function getArtistTop(param){ 
-    let t = await fetch("https://api.deezer.com/artist/" + param + "/top")
+    let t = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/" + param + "/top")
     let r = await t.json()
     return r.data
 }        
 export async function getArtistRadio(param){ 
-    let t = await fetch("https://api.deezer.com/artist/" + param + "/radio")
+    let t = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/" + param + "/radio")
     let r = await t.json()
     return r.data
 }
 export async function getArtistComments(param){ 
-    let t = await fetch("https://api.deezer.com/artist/" + param + "/comments")
+    let t = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/" + param + "/comments")
     let r = await t.json()
     return r.data
 }
