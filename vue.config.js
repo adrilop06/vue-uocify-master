bdbb68ca577647b6b5e0e9d485b2aabcdff40101
Adrian Lopez <adrianlopezpazos@gmail.com>
2021-01-05 18:14:41 -0600

    a

--- vue.config.js
+++ vue.config.js
@@ -22,7 +22,7 @@ module.exports = {
     // configure the workbox plugin
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {  
-      swSrc: 'ServiceWorker.js',
+      swSrc: 'registerServiceWorker.js',
     },
     iconPaths:{
       favicon32: 'public/img/icons/favicon-32x32.png',
